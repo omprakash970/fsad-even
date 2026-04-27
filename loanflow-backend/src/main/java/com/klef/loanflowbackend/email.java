2124ff4461a0025ffcf6938080619da78fc89165
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-04-27 22:10:14 +0530

    Fix production deployment: update application-prod.properties with correct config and remove deprecated email.java

--- loanflow-backend/src/main/java/com/klef/loanflowbackend/email.java
+++
@@ -1,8 +0,0 @@
-package com.klef.loanflowbackend;
-
-/**
- * @deprecated This file is deprecated. Use EmailController.java instead.
- */
-public class email {
-    // This class is empty and deprecated
-}
